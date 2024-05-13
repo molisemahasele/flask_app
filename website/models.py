2bcb47b42268eb3e1402d2d17a3415f8d169b5b6
Molise <61286622+molisemahasele@users.noreply.github.com>
2024-05-13 12:45:49 +0000

    Update models.py

--- website/models.py
+++ website/models.py
@@ -14,3 +14,17 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+    events = db.relationship('Event', backref='user')
+    
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime(timezone=True))
+    event_type = db.Column(db.String(50))  # Assuming event type has a limited range of options
+    reason = db.Column(db.String(255))  # Reason might be lengthy
+    operator = db.Column(db.String(50))  # Assuming operator has a limited range of options
+    capacity = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    
+    # Additional attributes if needed, like duration for breakdowns etc.
+
